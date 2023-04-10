0c956a27511d1a50ee14b594c754e9341fa69439
SerenityInAllThings <petersonvgama@gmail.com>
2023-04-10 10:28:40 -0300

    Patching node-ytdl-core version

--- postInstall.sh
+++ postInstall.sh
@@ -1,2 +1,3 @@
 # This needs to be here to I can run the app on railway
-npm install https://github.com/ShantanuNair/node-ytdl-core#v4.11.3-patch.1 --no-save
+# npm install https://github.com/ShantanuNair/node-ytdl-core#v4.11.3-patch.1 --no-save
+echo 'test'
