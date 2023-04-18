099edfec3a740713fc1a7a809f38ea593bca9d07
SerenityInAllThings <petersonvgama@gmail.com>
2023-04-18 11:23:51 -0300

    Added discordjs voice dependencies to startup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { createDJ } from './domain/dj';
+import { generateDependencyReport } from '@discordjs/voice'
 
+console.log('Discordjs voice dependencies:', generateDependencyReport());
 createDJ()
 // TODO: start webserver too
